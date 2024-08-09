978af74f44afba6b0a4c67b6a4421e459795a68d
MoldavskayaJabka <koomarik2007@gmail.com>
2024-08-09 15:36:44 +0300

    Add files via upload

---
+++ 123asd.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    setlocale(LC_ALL, "Ru");
+    cout << "Введите высоту треугольника: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < (2 * i - 1); k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
